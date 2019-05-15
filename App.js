96f772a079ffb724c25e25abd209d7765959d543
Wut Hmone Hnin Hlaing <wut.hmone@tmrnd.com.my>
2019-05-15 15:12:15 +0800

    done for tab

--- App.js
+++ App.js
@@ -12,9 +12,27 @@ export default class App extends React.Component {
                  screenBackgroundColor={{ backgroundColor: '#e9e3e3' }}
              >
                <View>
-                   {/*Page Content*/}
+                   {/*Page Content*/
+                    <Text> Still under development</Text>
+                   }
                </View>
              </TabBar.Item>
+
+             <TabBar.Item
+                 icon={require('./assets/brightness.png')}
+                 selectedIcon={require('./assets/brightness.png')}
+                 title="Tab3"
+                 screenBackgroundColor={{ backgroundColor: '#e9e3e3' }}
+             >
+               <View style = {{justifyContent: 'center', alignItems: 'center',lineHeight:13,}}>
+                   {/*Page Content*/
+
+                     <Text> Wut</Text>
+
+                   }
+               </View>
+             </TabBar.Item>
+
              <TabBar.Item
                  icon={require('./assets/sliders.png')}
                  selectedIcon={require('./assets/sliders.png')}
@@ -25,26 +43,19 @@ export default class App extends React.Component {
                      {/*Page Content*/}
                  </View>
              </TabBar.Item>
-             <TabBar.Item
-                 icon={require('./assets/brightness.png')}
-                 selectedIcon={require('./assets/brightness.png')}
-                 title="Tab3"
-                 screenBackgroundColor={{ backgroundColor: '#e9e3e3' }}
-             >
-               <View>
-                   {/*Page Content*/}
-               </View>
-             </TabBar.Item>
            </TabBar>
        );
      }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  text_center: {
     flex: 1,
-    backgroundColor: '#fff',
+    fontSize: 20,
+    lineHeight:13,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    fontWeight: 'bold',
+    color: 'black'
+  }
 });
